9603bb57aad5fbd4dc0f51d2b882ccd809cbb1eb
Suyash-Belwalkar <suyashbel0703@gmail.com>
2024-01-28 01:08:03 +0530

    folder struct setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import{Routes,Route} from 'react-router-dom';
+import Home from './containers/home1';
+import About from './containers/about';
+import Contact from './containers/contact';
+import Portfolio from './containers/portfolio';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*particle js*/}
+
+      {/* navBar */}
+
+      {/* main page content*/}
+
+      <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route  path='/about' element={<About/>}/>
+        <Route  path='/contact' element={<Contact/>}/>
+        <Route  path='/portfolio' element={<Portfolio/>}/>
+        <Route  path='/resume' element={<Resume/>}/>
+        <Route  path='/skills' element={<Skills/>}/>
+      </Routes>
     </div>
   );
 }
